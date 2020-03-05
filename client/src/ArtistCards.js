0c7efbce557dc8aba0abeba962cf0fc4d30aaf8f
Catie Evans <catieevans@Caties-MacBook-Pro.local>
2020-03-05 12:50:50 -0500

    adds horizontal scrolling artist cards

--- client/src/ArtistCards.js
+++ client/src/ArtistCards.js
@@ -1,29 +1,35 @@
-import React from 'react';
-
 class ArtistCards extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      names: '',
+      currentArtist: '',
     }
   };
   
   render() {
     return (
-      <div>
+      <div className='container-fluid ml-4 mb-5'>
+       <div className='row artist-row flex-row flex-nowrap overflow-auto'>
         {
           this.props.topTenArtist.map((artist,index) => {
             return (
-              <div>
-                <img src={artist.images[1].url}/>
-                <p key={artist.id}>{artist.name}</p>
+              <div className='col-4'>
+                <div className='card artist-card card-block'>
+                  <img className='img-card rounded-circle p-2' src={artist.images[1].url}/>
+                  <p className='artist-name' key={artist.id}>{artist.name}</p>
+                  <button className='btn btn-outline-light m-3 upcoming-shows-btn' data-toggle="modal" data-target="#exampleModal" onClick={() => this.props.artistEvents(artist)}>View upcoming shows</button>
+                </div>
               </div>
+        
             )
           })
         }
+        </div>
       </div>
     );
+
   }  
+  
 }
 
 export default ArtistCards;
